85ce0dfdb6d7caa55f144212ffcb75b80d00b6dd
kuba9605 <kuba9605@gmail.com>
2017-04-25 15:04:41 +0200

    invoice - elementy dodane

--- src/Invoice.java
+++ src/Invoice.java
@@ -2,5 +2,12 @@
  * Created by Administrator on 2017-04-11.
  */
 public class Invoice {
-
+    String id;
+    String typeA;
+    String typeB;
+    String product;
+    int amount;
+    float value;
+    float tax;
+    int clientid;
 }
