4d5ac9e7e67cf1c6c426bfde686b16a58977284f
amazinghat <olek.gawin@gmail.com>
2017-05-08 21:03:27 +0200

    Ciag dalszy raportowania

--- src/Invoice.java
+++ src/Invoice.java
@@ -27,12 +27,33 @@ public class Invoice {
           File raportfile = new File("raport.txt");
           PrintWriter zapis = new PrintWriter(new FileWriter(raportfile, true));
           String [][] raport = Communication.receive();
+          float przychod = 0, wydatek = 0, saldo, podatek = 0;
+          int iloscIncome = 0, iloscOutcome = 0;
 
+          //-------------------------ZESTAWIENIE TRANSAKCJI------------------------
           for (int i = 0; i < raport.length ; i++) {
              zapis.print("Transakcja " + i + ": " + raport[i][0] + " " + raport[i][1] + " " + raport[i][2] + " " + raport[i][3] + " " + raport[i][4] + " " + raport[i][5] + " " + raport[i][6] + " " + raport[i][7]);
              zapis.println();
+
+             if(raport[i][5] == "Income"){
+                iloscIncome++;
+                przychod = przychod + Float.parseFloat(raport[i][2]);
+             }
+             else if(raport[i][5] == "Outcome"){
+                iloscOutcome++;
+                wydatek = wydatek + Float.parseFloat(raport[i][2]);
+             }
+
+             podatek = podatek + Float.parseFloat(raport[i][3]);
           }
+          saldo = przychod - wydatek;
           zapis.println("Ilosc transakcji: " + raport.length);
+          zapis.println("Saldo: " + saldo);
+          zapis.println("Ilosc wplywow: " + iloscIncome);
+          zapis.println("Ilosc wydatkow: " + iloscOutcome);
+          zapis.println("Sumaryczny podatek: " + podatek);
+          zapis.println();
+          //------------------------------------------------------------------------
           zapis.close();
 
           Communication.delete();      // ---> czysci baze danych po raporcie
